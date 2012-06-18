e6dfbb4a74e9ad2f950e003a44715ce15d9caed1
Dave Benvenuti <dave@backupify.com>
2012-06-18 12:55:50 -0400

    don't overwrite existing uuids in payloads

--- lib/resque/plugins/resque_uuid/resque_extensions.rb
+++ lib/resque/plugins/resque_uuid/resque_extensions.rb
@@ -11,13 +11,24 @@ module Resque
         end
 
         def push(queue, item)
-          item[:uuid] = UUIDTools::UUID.random_create.to_s if item.respond_to?(:[]=)
+          uuid_generated = false
+
+          old_uuid = (item[:uuid] || item['uuid']).to_s.strip
+
+          # don't overwrite existing uuids
+          if item.respond_to?(:[]=) && (old_uuid.size == 0)
+            item[:uuid] = UUIDTools::UUID.random_create.to_s
+            uuid_generated = true
+          end
+
 
           ret = push_without_uuid(queue, item)
 
           # TODO (davebenvenuti 6/17/2012) the resque push method has no knowledge of the payload class, so this seems a bit sloppy.  However, I couldn't think of a better way to do it
-          payload_class = Util.constantize(item[:class] || item['class']) rescue nil
-          payload_class.after_uuid_generated(item[:uuid], *item[:args]) if payload_class && payload_class.respond_to?(:after_uuid_generated)
+          if uuid_generated
+            payload_class = Util.constantize(item[:class] || item['class']) rescue nil
+            payload_class.after_uuid_generated(item[:uuid], *item[:args]) if payload_class && payload_class.respond_to?(:after_uuid_generated)
+          end
 
           ret
         end
--- test/resque/plugins/resque_uuid/resque_extensions_test.rb
+++ test/resque/plugins/resque_uuid/resque_extensions_test.rb
@@ -67,4 +67,19 @@ class ResqueExtensionsTest < Test::Unit::TestCase
     assert_equal({ 'class' => FakeJobClassNoUUIDCallback.to_s, 'uuid' => test_uuid.to_s }, Resque.pop(:my_queue))
   end
 
+  should "not overwrite an existing uuid" do
+    UUIDTools::UUID.expects(:random_create).never
+
+    Resque.push :my_queue, { :class => FakeJobClassNoUUIDCallback.to_s, :uuid => 'my_unique_id' }
+
+    assert_equal({ 'class' => FakeJobClassNoUUIDCallback.to_s, 'uuid' => 'my_unique_id' }, Resque.pop(:my_queue))
+  end
+
+  should "not call after_uuid_generated if a new uuid wasn't generated" do
+    test_uuid = UUIDTools::UUID.random_create
+    FakeJobClass.expects(:after_uuid_generated).never
+
+    Resque.push :my_queue, { :class => FakeJobClassNoUUIDCallback.to_s, 'uuid' => 'my_unique_id' }
+  end
+
 end
